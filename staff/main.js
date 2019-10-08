540e3be5c0cff72739f31a69f032fe5bcc5f1387
Philip <philip.kheav@hotmail.com>
2019-10-08 05:46:16 +0000

    [Staff UI] CI UAT build 2019-10-08 05:46:15

--- staff/main.js
+++ staff/main.js
@@ -21050,7 +21050,7 @@ var version = '0.17.0';
 /** Version number of the base application */
 var core_version = '0.17.0';
 /** Build time of the application */
-var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1570491425000);
+var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1570513412000);
 
 
 /***/ }),
