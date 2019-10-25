53de0766bc836cbce0ad8b844ecdac625fabf6c9
Ben <ben@benhoad.net>
2019-10-25 03:14:21 +0000

    [Concierge UI] CI UAT build 2019-10-25 03:14:21

--- concierge/main-es2015.js
+++ concierge/main-es2015.js
@@ -17616,7 +17616,7 @@ const version = '0.4.0';
 /** Version number of the base application */
 const core_version = '0.4.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1571963893000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1571973067000);
 
 
 /***/ }),
