5a1c633092e9a43eccc506b7138e2a2555b6edfd
Ben <ben@benhoad.net>
2019-11-01 05:13:27 +0000

    [Staff UI] CI UAT build 2019-11-01 05:13:27

--- staff/main.js
+++ staff/main.js
@@ -21137,7 +21137,7 @@ var version = '0.17.0';
 /** Version number of the base application */
 var core_version = '0.17.0';
 /** Build time of the application */
-var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1572583916000);
+var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1572585036000);
 
 
 /***/ }),
