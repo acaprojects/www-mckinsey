b227d5ff430a48fd5248e453a912521adf86626e
Alex <alex@yuion.net>
2019-11-28 22:49:43 +0000

    [Concierge UI] CI UAT build 2019-11-28 22:49:43

--- concierge/main-es2015.js
+++ concierge/main-es2015.js
@@ -17909,7 +17909,7 @@ const version = '0.4.0';
 /** Version number of the base application */
 const core_version = '0.4.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1574980046000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1574981183000);
 
 
 /***/ }),
