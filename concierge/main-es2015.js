f0649ee840854348316a4f64f398e4647ba9e831
Alexandre <alexandre@acaprojects.com>
2019-12-10 01:59:48 +0000

    [Concierge UI] CI UAT build 2019-12-10 01:59:48

--- concierge/main-es2015.js
+++ concierge/main-es2015.js
@@ -17951,7 +17951,7 @@ const version = '0.4.0';
 /** Version number of the base application */
 const core_version = '0.4.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1575855773000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1575942974000);
 
 
 /***/ }),
--- concierge/main-es5.js
+++ concierge/main-es5.js
@@ -21507,7 +21507,7 @@ var version = '0.4.0';
 /** Version number of the base application */
 var core_version = '0.4.0';
 /** Build time of the application */
-var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1575855773000);
+var build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1575942974000);
 
 
 /***/ }),
