449cd7e7f2d85bb99a559bf4b100ce217c003725
Philip <philip.kheav@hotmail.com>
2019-12-16 15:19:39 +0000

    [Staff UI] CI UAT build 2019-12-16 15:19:39

--- staff/main-es2015.js
+++ staff/main-es2015.js
@@ -19302,7 +19302,7 @@ const version = '0.17.0';
 /** Version number of the base application */
 const core_version = '0.17.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576453546000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576509325000);
 
 
 /***/ }),
