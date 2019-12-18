01c1db9b2173add7d7b08a1afac1389bc7e5b771
Alexandre <alexandre@acaprojects.com>
2019-12-18 22:56:10 +0000

    [Staff UI] CI UAT build 2019-12-18 22:56:10

--- staff/main-es2015.js
+++ staff/main-es2015.js
@@ -19312,7 +19312,7 @@ const version = '0.17.0';
 /** Version number of the base application */
 const core_version = '0.17.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576639873000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576709544000);
 
 
 /***/ }),
