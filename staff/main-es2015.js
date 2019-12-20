deab2ec8f347b6f81bc552ffa0ea8ada84ccb7da
Viv <viv@acaprojects.com>
2019-12-20 05:19:49 +0000

    [Staff UI] CI UAT build 2019-12-20 05:19:49

--- staff/main-es2015.js
+++ staff/main-es2015.js
@@ -19313,7 +19313,7 @@ const version = '0.17.0';
 /** Version number of the base application */
 const core_version = '0.17.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576799750000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1576818962000);
 
 
 /***/ }),
