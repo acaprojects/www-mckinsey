b5524ed30b8fe875071a16b982373a87095e7768
Alex <alex@yuion.net>
2020-03-06 04:54:22 +0000

    [Staff UI] CI UAT build 2020-03-06 04:54:22

--- staff/main-es2015.js
+++ staff/main-es2015.js
@@ -19461,7 +19461,7 @@ const version = '0.17.0';
 /** Version number of the base application */
 const core_version = '0.17.0';
 /** Build time of the application */
-const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1583469577000);
+const build = dayjs__WEBPACK_IMPORTED_MODULE_0__(1583470224000);
 
 
 /***/ }),
