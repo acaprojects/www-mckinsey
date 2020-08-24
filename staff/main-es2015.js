965d82afcac93ab6b13900fbf30aef164937cfc3
polonski <polonski@users.noreply.github.com>
2020-08-24 03:02:21 +0000

    build(staff): CI build from 27e739d

--- staff/main-es2015.js
+++ staff/main-es2015.js
@@ -21705,16 +21705,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* tslint:disable */
 exports.VERSION = {
     "dirty": false,
-    "raw": "427089f",
-    "hash": "427089f",
+    "raw": "27e739d",
+    "hash": "27e739d",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "427089f",
+    "suffix": "27e739d",
     "semverString": null,
     "version": "0.0.0",
     "core_version": "1.0.0",
-    "time": 1597970846511
+    "time": 1598237884431
 };
 /* tslint:enable */
 
--- staff/main-es5.js
+++ staff/main-es5.js
@@ -38675,16 +38675,16 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
     exports.VERSION = {
       "dirty": false,
-      "raw": "427089f",
-      "hash": "427089f",
+      "raw": "27e739d",
+      "hash": "27e739d",
       "distance": null,
       "tag": null,
       "semver": null,
-      "suffix": "427089f",
+      "suffix": "27e739d",
       "semverString": null,
       "version": "0.0.0",
       "core_version": "1.0.0",
-      "time": 1597970846511
+      "time": 1598237884431
     };
     /* tslint:enable */
 
