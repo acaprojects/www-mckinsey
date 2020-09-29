61f2bd8119a069c78022911009b25d6a31b1aa77
pkheav <pkheav@users.noreply.github.com>
2020-09-29 13:36:31 +0000

    build(staff): CI build from 9e3c99b

--- staff/main-es5.js
+++ staff/main-es5.js
@@ -39522,16 +39522,16 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
     exports.VERSION = {
       "dirty": false,
-      "raw": "802ecd3",
-      "hash": "802ecd3",
+      "raw": "9e3c99b",
+      "hash": "9e3c99b",
       "distance": null,
       "tag": null,
       "semver": null,
-      "suffix": "802ecd3",
+      "suffix": "9e3c99b",
       "semverString": null,
       "version": "0.0.0",
       "core_version": "1.0.0",
-      "time": 1601357285615
+      "time": 1601386278321
     };
     /* tslint:enable */
 
